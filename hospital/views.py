ae1e1d74d3c5754c09d753cfcea8472774d978dc
boghas <bogdan.hasan@mindit.io>
2022-04-04 10:31:02 +0300

    Fixed a bug on admin_login page

--- hospital/views.py
+++ hospital/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def About(request):
@@ -9,4 +11,35 @@ def Home(request):
 
 def Contact(request):
     return render(request, 'contact.html')
+
+def Index(request):
+    if not (request.user.is_staff):
+        return redirect('login')
+    return render(request, 'home.html')
+
+def Login(request):
+    error = ""
+    if(request.method == "POST"):
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username=u, password=p)
+        try:
+            if(user.is_staff):
+                Login(request, user)
+                error = 'no'
+            else:
+                error = "yes"
+        except:
+            error = "yes"
+    d = {'error': error}
+    return render(request, 'login.html', d)
+
+def Logout_admin(request):
+    if not (request.user.is_staff):
+        return redirect('login')
+
+    logout(request)
+    return redirect('login')
+
+
 
